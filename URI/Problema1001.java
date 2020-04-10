2c6c438b1f9ed85605cf796c5d5f6b0b0bdd0c56
anaxiel-code <lms.pereiraa@gmail.com>
2020-04-10 12:55:48 -0300

    Problema1001

--- URI/Problema1001.java
+++ URI/Problema1001.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Problema1001 
-{    public static void main (String[] args)throws IOException
+{    public static void main (String[] args) throws IOException
     {   
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
